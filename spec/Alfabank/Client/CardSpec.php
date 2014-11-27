2d71f79311d0ef4e5758077406056f5057b09d44
Stepan Anchugov <kixxx1@gmail.com>
2014-11-27 19:32:52 +0500

    Added cards

--- spec/Alfabank/Client/CardSpec.php
+++ spec/Alfabank/Client/CardSpec.php
@@ -7,6 +7,12 @@ use Prophecy\Argument;
 
 class CardSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->beConstructedWith('1111222233334444');
+    }
+
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Alfabank\Client\Card');
--- src/Alfabank/Client/Card.php
+++ src/Alfabank/Client/Card.php
@@ -4,4 +4,23 @@ namespace Alfabank\Client;
 
 class Card
 {
+
+    /**
+     * @var string
+     */
+    private $number;
+
+    public function __construct($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
 }
